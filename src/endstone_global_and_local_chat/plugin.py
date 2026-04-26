e2c3efca67b428919ead56f124a58c20bd812a26
niko-at-chalupa <181960037+niko-at-chalupa@users.noreply.github.com>
2026-04-26 21:55:11 +0800

    Implement global chat

--- src/endstone_global_and_local_chat/plugin.py
+++ src/endstone_global_and_local_chat/plugin.py
@@ -7,3 +7,16 @@ class GlobalAndLocalChat(Plugin):
 
     def on_enable(self):
         self.register_events(self)
+
+    @event_handler
+    def on_player_chat(self, event: PlayerChatEvent):
+        event.cancel()
+        
+        message = event.message
+
+        if message[0] == "!":
+            message = message[1:]
+            message = f"[{cf.GREEN}GLOBAL{cf.RESET}] <{event.player.name}> {message}"
+            self.server.broadcast_message(message)
+        else:
+            event.player.send_message(f"{cf.ITALIC}{cf.GRAY}No one heard you...")
