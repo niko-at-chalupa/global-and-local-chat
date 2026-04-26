e5ddfe7ce91658699b274e8cecf67247366ca2eb
niko-at-chalupa <181960037+niko-at-chalupa@users.noreply.github.com>
2026-04-26 22:08:38 +0800

    Implement local chat

--- src/endstone_global_and_local_chat/plugin.py
+++ src/endstone_global_and_local_chat/plugin.py
@@ -1,5 +1,6 @@
 from endstone.plugin import Plugin
 from endstone.event import PlayerChatEvent, event_handler
+from endstone import Player
 from endstone import ColorFormat as cf
 
 class GlobalAndLocalChat(Plugin):
@@ -7,6 +8,20 @@ class GlobalAndLocalChat(Plugin):
 
     def on_enable(self):
         self.register_events(self)
+
+    def get_players_in_radius(self, player: Player, radius: int) -> list[Player]:
+        nearby_players = []
+        
+        for other_player in self.server.online_players:
+            if other_player == player:
+                continue
+            
+            if (abs(other_player.location.x - player.location.x) <= radius and
+                abs(other_player.location.y - player.location.y) <= radius and
+                abs(other_player.location.z - player.location.z) <= radius):
+                nearby_players.append(other_player)
+        
+        return nearby_players
 
     @event_handler
     def on_player_chat(self, event: PlayerChatEvent):
@@ -19,4 +34,10 @@ class GlobalAndLocalChat(Plugin):
             message = f"[{cf.GREEN}GLOBAL{cf.RESET}] <{event.player.name}> {message}"
             self.server.broadcast_message(message)
         else:
-            event.player.send_message(f"{cf.ITALIC}{cf.GRAY}No one heard you...")
+            recipients = self.get_players_in_radius(event.player, 100)
+            if len(recipients) == 0:
+                event.player.send_message(f"{cf.ITALIC}{cf.GRAY}No one heard you...")
+                return
+            message = f"[{cf.YELLOW}LOCAL{cf.RESET}] <{event.player.name}> {event.message}"
+            for recipient in recipients:
+                recipient.send_message(message)
